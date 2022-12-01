36d1eb96ae050b4dc9165aba721a5b5bef8a2cc7
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-12-01 08:46:02 -0500

    initial commit

---
+++ calculator/src/components/DigitButton.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const DigitButton = (props) => {
+    return (
+        <div id={props.id}>
+            {props.button}
+        </div>
+    )
+}
+
+export default DigitButton
---
+++ calculator/src/components/OperationButton.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const OperationButton = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default OperationButton
